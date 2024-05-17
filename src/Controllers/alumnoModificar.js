96a408f8ef93d8888108d270221776a2474ea0bf
TomasBozzano <tomycas10@gmail.com>
2024-05-17 00:19:05 -0300

    Mejoras de como recorre los datos para luego modificarlos de la manera correcta

--- src/Controllers/alumnoModificar.js
+++ src/Controllers/alumnoModificar.js
@@ -1,57 +1,50 @@
 const conexion = require('../Database/conexion');
 
 const modificarAlumnos = async (req, res) => {
-    const datosAlumnos = req.body.alumnosDatos;
+    const datosAlumnos = req.body.alumnos;
 
-    if (!datosAlumnos || Object.keys(datosAlumnos).length === 0) {
+    if (!datosAlumnos || datosAlumnos.length === 0) {
         return res.status(400).json({ error: 'Datos de alumnos no válidos' });
     }
-
     // Arreglo para almacenar promesas de consultas SQL
     const consultas = [];
+    for (const datos of datosAlumnos){
+        const codigoMateria = datos.codigoMateria;
+        for (const alumno of datos.alumnos){
+            const { Permiso, Parcial1, Parcial2, Totalizador, Recuperatorio1, Recuperatorio2, Practico1, Practico2, Practico3, Practico4, Practico5, Cursada, Asistencia } = alumno;
 
-    // Iterar sobre cada alumno y agregar la consulta SQL al arreglo de promesas
-    for (const permiso in datosAlumnos) {
-        const { Datos } = datosAlumnos[permiso];
-        const { Parcial1, Parcial2, Totalizador, Recuperatorio1, Recuperatorio2, Practico1, Practico2, Practico3, Practico4, Practico5, Cursada, Asistencia, codigoMateria } = Datos;
-
-        const query = `
-            UPDATE Finales
-            SET 
-                f.Parcial1 = ${Parcial1}, 
-                f.Parcial2 = ${Parcial2}, 
-                f.Totalizador = ${Totalizador}, 
-                f.Recuperatorio1 = ${Recuperatorio1}, 
-                f.Recuperatorio2 = ${Recuperatorio2}, 
-                f.Practico1 = ${Practico1}, 
-                f.Practico2 = ${Practico2}, 
-                f.Practico3 = ${Practico3}, 
-                f.Practico4 = ${Practico4}, 
-                f.Practico5 = ${Practico5}, 
-                f.Cursada = ${Cursada}, 
-                f.Asistencia = ${Asistencia}
-            FROM Finales f,
-            Alumnos a,
-            Materias m
-            WHERE f.Alumnno = ${permiso}
-            AND f.Alumno = a.Permiso
-            AND f.Materia = m.Codigo
-            AND f.Ano = (Select AñoMatriculacion from Parametros)
-            AND f.Materia = ${codigoMateria}`;
-
-        // Agregar la consulta al arreglo de promesas
-        consultas.push(conexion.query(query));
+            const query = `
+                UPDATE Finales
+                SET 
+                    Parcial1 = ${Parcial1}, 
+                    Parcial2 = ${Parcial2}, 
+                    Totalizador = ${Totalizador}, 
+                    Recuperatorio1 = ${Recuperatorio1}, 
+                    Recuperatorio2 = ${Recuperatorio2}, 
+                    Practico1 = ${Practico1}, 
+                    Practico2 = ${Practico2}, 
+                    Practico3 = ${Practico3}, 
+                    Practico4 = ${Practico4}, 
+                    Practico5 = ${Practico5}, 
+                    Cursada = ${Cursada}, 
+                    Asistencia = ${Asistencia}
+                WHERE 
+                    Alumno = ${Permiso}
+                    AND Materia = ${codigoMateria}
+                    AND Ano = (SELECT AñoMatriculacion FROM Parametros)
+            `;
+            consultas.push(conexion.query(query));
+        }
     }
 
-    // Ejecutar todas las consultas en paralelo
-    Promise.all(consultas)
-        .then(() => {
-            res.status(200).json({ mensaje: 'Datos actualizados exitosamente' });
-        })
-        .catch((error) => {
-            console.error(query);
-            res.status(500).json({ error: 'Error en la consulta', details: error.message });
-        });
+    try {
+        await Promise.all(consultas);
+        res.status(200).json({ mensaje: 'Datos actualizados exitosamente' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error en la consulta', details: error.message });
+    }
 };
+
 
 module.exports = modificarAlumnos;
